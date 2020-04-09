bf529060c6a8890a61b066b1206ae475367ad377
Gabe <gperry22@gmail.com>
2020-04-09 19:19:12 -0400

    new db

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const routes = require('./routes');
+const express = require("express");
+const mongoose = require("mongoose");
+const routes = require("./routes");
 const app = express();
 
 const PORT = process.env.PORT || 3001;
@@ -8,26 +8,24 @@ const PORT = process.env.PORT || 3001;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-var databaseToUse = ""
+var databaseToUse = "";
 
 if (process.env.NODE_ENV === "production") {
-	app.use(express.static('client/build'));
-	databaseToUse = "mongodb://gabe:a123456@ds133231.mlab.com:33231/heroku_5vfx1n8w";
+  app.use(express.static("client/build"));
+  databaseToUse =
+    "mongodb://gabe:a123456@ds129010.mlab.com:29010/heroku_qpsq8tsw";
+} else {
+  databaseToUse = "mongodb://localhost/reactBoilerplate";
 }
-else {
-	databaseToUse = 'mongodb://localhost/reactBoilerplate';
-}
-
 
 app.use(routes);
 
 const MONGODB_URI = process.env.MONGODB_URI || databaseToUse;
 
-
 mongoose.Promise = global.Promise;
 
 mongoose.connect(MONGODB_URI);
 
-app.listen(PORT, function() {
-	console.log(`App running on port ${PORT}`);
+app.listen(PORT, function () {
+  console.log(`App running on port ${PORT}`);
 });
